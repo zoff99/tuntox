07181584282548bf8d5d4929f39822eaa05939d1
zoff99 <zoff99@users.noreply.github.com>
2024-06-29 09:25:38 +0200

    fix gitversion.h

--- gitversion.h
+++ gitversion.h
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-#define GITVERSION "f302b2b93461080888cd9c26ec78d33732c8a0e7"
-=======
-#define GITVERSION "d20725aee450754cc86e8fa1410bbb591561eeda"
->>>>>>> upstream/master
+#define GITVERSION "65523727923a58653563bffd66c9943ca2dc6ae0"
